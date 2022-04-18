5107aa0a149cf1e8667214b668867c0cf21eedd6
George Kaluwin <georgekaluwin@gmail.com>
2022-04-18 23:24:38 +0930

    search displays current weather

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,37 @@
 var button=document.querySelector('.button')
 var inputValue=document.querySelector('.inputValue')
-var name=document.querySelector('.name')
+var cityName=document.querySelector('.cityName')
 
 // variables to be displayed
-var name=document.querySelector('.name')
+var cityName=document.querySelector('.cityName')
 var description=document.querySelector('.description')
-var temperature=document.querySelector('.temp')
-var humidity=document.querySelector('.humid')
+var temp=document.querySelector('.temp')
+var humid=document.querySelector('.humid')
 var wind=document.querySelector('.wind')
 var uv=document.querySelector('.uv')
 
 // event listener for button click
 button.addEventListener('click', function(){
     // our API key from OpenWeather
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d181a9b55d72cfbb2265fd5aaf6c7c54')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d181a9b55d72cfbb2265fd5aaf6c7c54&units=metric')
 .then(response => response.json())
-.then(data => console.log(data))
+// .then(data => console.log(data))
 
-.catch(err => alert("incorrect city name"))
+.then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descriptionValue = data['weather'][0]['description'];
+    var windValue = data['wind']['speed'];
+    var humidValue = data['main']['humidity'];
+
+    cityName.innerHTML=nameValue
+    temp.innerHTML = tempValue + " degrees Celcius";
+    description.innerHTML= "Description: " + descriptionValue;
+    wind.innerHTML= "wind speed: " + windValue;
+    humid.innerHTML= "humidity: " + humidValue;
+
+})
+
+
+// .catch(err => alert("incorrect city name"))
 })
