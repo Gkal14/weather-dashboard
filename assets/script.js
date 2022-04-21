a1b215e4313519a467aed9cd6932e3690f79e018
George Kaluwin <georgekaluwin@gmail.com>
2022-04-21 17:36:08 +0930

    JS fixes

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,13 @@ var cityName= document.querySelector('.cityName')
 
 // variables to be displayed
 var cityName= document.querySelector('.cityName')
+var today = document.querySelector('#today')
+var date = new Date()
+var dd = String(date.getDate()).padStart(2, '0')
+var mm = String(date.getMonth() + 1).padStart(2, '0')
+var yyyy = date.getFullYear()
+date = dd + '/' + mm + '/' + yyyy
+
 var description= document.querySelector('.description')
 var icon= document.querySelector('.icon')
 var temp= document.querySelector('.temp')
@@ -13,6 +20,20 @@ var uv= document.querySelector('.uv')
 
 // event listener for button click
 button.addEventListener('click', function(){
+
+    //Show hidden fields once user has searched
+    var weatherToday = document.querySelector('#weather-today');
+    var fiveDay = document.querySelector('#five-day');
+    var searchHistory = document.querySelector('#searchHistory');
+
+    weatherToday.classList.remove('hide');
+    fiveDay.classList.remove('hide');
+    searchHistory.classList.remove('hide');
+
+    weatherToday.classList.add('show');
+    fiveDay.classList.add('show');
+    searchHistory.classList.add('show');
+
     // our API key from OpenWeather
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d181a9b55d72cfbb2265fd5aaf6c7c54&units=metric')
 .then(response => response.json())
@@ -27,15 +48,15 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&ap
     var humidValue = data['main']['humidity'];
 
     cityName.innerHTML= nameValue;
+    today.innerHTML = date;
     temp.innerHTML = tempValue + " degrees Celcius";
-    description.innerHTML= "Description: " + descriptionValue;
 
     // Section to determine what icon to display
     if (descriptionValue == 'Clouds'){
         icon.src = 'img/cloud.png';
     } else if (descriptionValue == 'Clear'){
         icon.src = 'img/sun.png';
-    } else if (descriptionValue == 'Rainy'){
+    } else if (descriptionValue == 'Rain'){
         icon.src = 'img/rain.png';
     }
 
@@ -69,11 +90,49 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&ap
             uv.classList.remove('green-background');
             uv.classList.remove('orange-background');
         }
+       
+        //Get 5 day forecast information
+        //I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+        //Loop through 1-5 weather forecast and display
+        for (let i = 1; i < 6; i++){
+
+            var newDate = new Date()
+            newDate.setDate(newDate.getDate()+i)
+            var dd = String(newDate.getDate()).padStart(2, '0')
+            var mm = String(newDate.getMonth() + 1).padStart(2, '0')
+            var yyyy = newDate.getFullYear()
+            newDate = dd + '/' + mm + '/' + yyyy
+
+            var date = document.querySelector('.date'+i)
+            var icon= document.querySelector('.icon'+i)
+            var temp= document.querySelector('.temp'+i)
+            var humid= document.querySelector('.humid'+i)
+            var wind= document.querySelector('.wind'+i)
+           
+            var tempValue = data['daily'][i]['temp']['max'];
+            //Change from kelvin to celsius
+            tempValue = tempValue - 273.15;
+            tempValue = tempValue.toFixed(2);
+            var descriptionValue = data['daily'][i]['weather'][0]['main'];
+            var windValue = data['daily'][i]['wind_speed'];
+            var humidValue = data['daily'][i]['humidity'];
+
+            // Section to determine what icon to display
+            if (descriptionValue == 'Clouds'){
+                icon.src = 'img/cloud.png';
+            } else if (descriptionValue == 'Clear'){
+                icon.src = 'img/sun.png';
+            } else if (descriptionValue == 'Rain'){
+                icon.src = 'img/rain.png';
+            }
+
+            date.innerHTML = newDate;
+            temp.innerHTML = tempValue;
+            wind.innerHTML= "Wind speed: " + windValue;
+            humid.innerHTML= "Humidity: " + humidValue;
+        }
 
     })
 
 })
-
-
-// .catch(err => alert("incorrect city name"))
 })
